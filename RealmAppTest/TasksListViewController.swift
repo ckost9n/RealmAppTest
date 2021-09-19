53ec4e96dc0b17a62a000ba7de835ce665b70945
ckost9n <ckost9n@gmail.com>
2021-09-19 18:31:30 +0300

    setup autoLayout for firstVC

---
+++ RealmAppTest/TasksListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  TasksListViewController.swift
+//  RealmAppTest
+//
+//  Created by Konstantin on 19.09.2021.
+//
+
+import UIKit
+
+class TasksListViewСontroller: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        alertForAddAndUpdateList()
+    }
+    @IBAction func sortingList(_ sender: UISegmentedControl) {
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
+// MARK: - Sorted
+
+extension TasksListViewСontroller {
+    
+    private func alertForAddAndUpdateList() {
+        
+        let alert = UIAlertController(title: "New List", message: "Please insert new value", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let text = alertTextField.text, !text.isEmpty else { return }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        alert.addTextField { textField in
+            alertTextField = textField
+            alertTextField.placeholder = "List Name"
+        }
+        
+        present(alert, animated: true)
+        
+    }
+    
+}
+
